d3fba7e6ee0c25d996609b75657928376b1e16e9
subahm6 <145737306+subahm6@users.noreply.github.com>
2025-12-04 09:55:21 +0530

    add fleet and calculator

--- AGNIRA web/app.py
+++ AGNIRA web/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/fleetowner')
+def fleetowner():
+    return render_template('fleetowner.html')
+
+@app.route('/freight-calculator')
+def freight_calculator():
+    return render_template('freightcalculator.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False) 
